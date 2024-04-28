16fffd1164cf5467fbc8b554691fce9f0bbf6d37
olfa lafi <olfalafi1998@gmail.com>
2024-04-28 01:33:04 +0100

    responsive form

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const registerLink = document.querySelector('.register-link')
 const loginLink= document.querySelector('.login-link')
 const btnPopUp= document.querySelector('.btn-login');
 const btnClose = document.querySelector('.icon-close');
+const iconBars= document.querySelector('.icon-bars');
+const headerRepsonsive= document.querySelector('header');
+const iconXmark = document.querySelector('.icon-xmark');
+const btnLogin= document.querySelector('.btn-login')
 registerLink.addEventListener('click', (e)=>{
         e.preventDefault();
         wrapper.classList.add('active')
@@ -16,6 +20,21 @@ btnPopUp.addEventListener('click', (e)=>{
     e.preventDefault();
     wrapper.classList.add('active-popup');
 })
-btnClose.addEventListener('click',(e)=>{
+btnClose.addEventListener('click',()=>{
     wrapper.classList.remove('active-popup')
+})
+iconBars.addEventListener('click',()=>{
+    console.log('object')
+    headerRepsonsive.classList.add('active-icon-bars')
+})
+iconXmark.addEventListener('click', ()=>{
+    headerRepsonsive.classList.remove('active-icon-bars')
+})
+btnLogin.addEventListener('click', (e)=>{
+    e.preventDefault();
+    wrapper.classList.add('form-active')
+})
+btnLogin.addEventListener('click', (e)=>{
+    e.preventDefault();
+    headerRepsonsive.classList.remove('active-icon-bars')
 })
